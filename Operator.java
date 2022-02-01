0650dcaea40acfb35d84a6128e9a38cc7ccb3417
SowmyaJHemanth <97523100+SowmyaJHemanth@users.noreply.github.com>
2022-02-02 01:36:55 +0530

    Add files via upload
    Day 1

---
+++ Operator.java
@@ -0,0 +1,39 @@
+class Operators{
+ public static void main(String arg[])
+	{
+		int sum = 100 + 50;//basic operation
+    		int sub = 10-5;
+	    	int prod = 2*5;
+	int div = 10/2;
+	int modu=25/5;
+    	System.out.println(sum);
+    	System.out.println(sub);
+    	System.out.println(prod);  
+  	System.out.println(div);  
+	System.out.println(mod);  
+
+	 int x = 10;//shortcut assignment
+  	  x += 5;
+	int x1-=5;
+	int x2*=5;
+	int x3/=5;
+	
+    	System.out.println(x);
+  	System.out.println(x1);
+	System.out.println(x2);
+	System.out.println(x3);
+  	
+	int x4 = 5; //comparison operator
+   	 int y4 = 3;
+ 	   System.out.println(x4 >= y4); 
+ 	 System.out.println(x4 <= y4); 
+	System.out.println(x4 == y4); 
+	System.out.println(x4 != y4); 
+
+	int x5 = 5; // logical opertors
+   	 System.out.println(x5 > 3 || x5 < 4);
+   	 System.out.println(x5 > 3 && x5 < 4);
+   	 System.out.println(!(x5 > 3));
+
+	}
+}
